db65ebc2f9a3536334895f7f01c605f1d449bb91
John Desrosiers <johndrunner@yahoo.com>
2018-05-12 16:15:34 -0700

    BrandSelector calls props.onBrandSelect.
    Filters brands to prevent repeating brands.
    Add tests to BrandSelector.test.jsx.
    Modify BrandSelector component.

--- src/components/BrandSelector.jsx
+++ src/components/BrandSelector.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function BrandSelector(props) {
+  const onlyUnique = (item, index, array) => array.indexOf(item) === index;
+  const brands = products => products.map(product => product.brand).filter(onlyUnique);
   return (
-    <select>
-      {props.brands.map(brand => <option key={brand} value={brand}>{brand}</option>)}
+    <select onChange={event => props.onBrandSelect(event.target.value)}>
+      {brands(props.products)
+          .map(brand => <option key={brand} value={brand}>{brand}</option>)}
     </select>
   );
 }
 
 BrandSelector.propTypes = {
-  brands: PropTypes.arrayOf(PropTypes.string).isRequired,
+  products: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onBrandSelect: PropTypes.func.isRequired,
 };
 
 export default BrandSelector;
--- src/components/BrandSelector.test.jsx
+++ src/components/BrandSelector.test.jsx
@@ -5,22 +5,33 @@ import BrandSelector from './BrandSelector';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+let mockProducts;
 let mockBrands;
 let wrapper;
+let brandSelectFn;
+
+const findAllOptions = () => wrapper.find('option');
+const createWrapper = () =>
+  shallow(<BrandSelector products={mockProducts} onBrandSelect={brandSelectFn} />);
 
 beforeEach(() => {
-  mockBrands = ['Nike', 'Adidas'];
-  wrapper = shallow(<BrandSelector brands={mockBrands} />);
+  mockProducts = [
+    { id: 1, name: 'Mock Product 1', brand: 'MockBrandA' },
+    { id: 2, name: 'Mock Product 2', brand: 'MockBrandB' },
+  ];
+  mockBrands = ['MockBrandA', 'MockBrandB'];
+  brandSelectFn = jest.fn();
+  wrapper = createWrapper();
 });
 
 it('should render a `<select>` element', () => {
   expect(wrapper.find('select').length).toEqual(1);
 });
 
-it('should render an `<option>` for each brand in props.brands', () => {
-  const selectWrapper = wrapper.find('option');
+it('should render an `<option>` for each brand in props.products', () => {
+  const options = findAllOptions();
   const optionIndex = 0;
-  const optionWrapper = selectWrapper.at(optionIndex);
+  const optionWrapper = options.at(optionIndex);
 
   function hasExpectedValue() {
     return expect(optionWrapper.prop('value')).toEqual(mockBrands[optionIndex]);
@@ -30,7 +41,27 @@ it('should render an `<option>` for each brand in props.brands', () => {
     return expect(optionWrapper.contains(mockBrands[optionIndex])).toBeTruthy();
   }
 
-  expect(selectWrapper.length).toEqual(mockBrands.length);
+  expect(options.length).toEqual(mockBrands.length);
   hasExpectedValue();
   containsBrandText();
+});
+
+it('should not repeat brands', () => {
+  mockProducts.push({ id: 3, name: 'Mock Product 3', brand: 'MockBrandA' });
+  wrapper = createWrapper();
+  expect(findAllOptions().length).toEqual(mockBrands.length);
+});
+
+describe('props.onBrandSelect()', () => {
+  it('should be called when brand `<select>` changes', () => {
+    const selectWrapper = wrapper.find('select');
+    selectWrapper.simulate('change', { target: { value: 'hello' } });
+    expect(brandSelectFn).toHaveBeenCalled();
+  });
+
+  it('should be called with the brand', () => {
+    const selectWrapper = wrapper.find('select');
+    selectWrapper.simulate('change', { target: { value: 'hello' } });
+    expect(brandSelectFn.mock.calls[0][0]).toEqual('hello');
+  });
 });
